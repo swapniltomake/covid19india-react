e855a5bba0186e84b9f3db7070929a4e21bd6c8b
swapniltomake <swapniltomake@rediffmail.com>
2020-05-06 23:54:54 +0000

    Deploying to gh-pages from  @ e963433f12c6f39af573db3a382400c746a7046d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.334754180d2090b2304f29d2e4cb17ba.js"
+  "/precache-manifest.a0325ee19daff44b6de6a7f7257295b7.js"
 );
 
 self.addEventListener('message', (event) => {
